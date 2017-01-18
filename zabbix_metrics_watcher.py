a8e30288675216677dd756adc32e7808c39390ee
Vadim Rutkovsky <vrutkovs@redhat.com>
2017-01-18 18:53:44 +0100

    Remove a limit on max_spent_in_new - send the zabbix message about it everytime

--- zabbix_metrics_watcher.py
+++ zabbix_metrics_watcher.py
@@ -29,7 +29,6 @@ Zabbix Items (generally sent when a build changes a state):
  * phase - name of the phase this build is in
  * state - 0 for any in progress state, 1 for complete (Complete / Failed / Cancelled)
  * <atomic-reactor plugin name> - how long did each plugin took to complete
- * <
  * pulp_push_speed - average speed of pulp push (or pulp_sync if pulp_push wasn't performed)
  * upload_size_mb - size of the image uploaded to koji
 
@@ -206,8 +205,6 @@ def filter_completed_builds(completed_builds):
 
 
 def run(zabbix_host, osbs_master, config, instance):
-    STUCK_IN_NEW_THRESHOLD_SECS = 5
-
     running_builds = set()
     builds_in_new = {}
     pending = set()
@@ -262,23 +259,21 @@ def run(zabbix_host, osbs_master, config, instance):
                     running_builds.add(build_name)
                 pending.discard(build_name)
 
-            elif (status in ['Running'] and changeset == 'deleted')\
+            elif (status == 'Running' and changeset == 'deleted')\
               or (status in ['Complete', 'Failed', 'Cancelled']):
-                try:
-                    running_builds.remove(build_name)
-                except Exception as e:
-                    logger.warn("Error while removing running build: %r", e)
+                pending.discard(build_name)
+                running_builds.discard(build_name)
+
             else:
                 logging.warn("Unhandled status: %r", status)
 
             spent_in_new = [(now - start).total_seconds() for start in builds_in_new.values()]
             if spent_in_new:
                 max_spent_in_new = max(spent_in_new)
-                if max_spent_in_new > STUCK_IN_NEW_THRESHOLD_SECS:
-                    logger.info("%s build(s) are in New for, longest: %s sec",
-                                len(builds_in_new), max_spent_in_new)
-                    _send_zabbix_message(zabbix_host, osbs_master,
-                                         "new_duration", max_spent_in_new)
+                logger.info("%s build(s) are in New for, longest: %s sec",
+                            len(builds_in_new), max_spent_in_new)
+                _send_zabbix_message(zabbix_host, osbs_master,
+                                     "new_duration", max_spent_in_new)
 
             build.send_zabbix_notification(zabbix_host, osbs_master, len(running_builds))
 
